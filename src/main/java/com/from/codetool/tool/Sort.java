da609e981d0ced573f5c9623905aa2d60f90d0ab
fromfield <liyanlongyan@163.com>
2017-12-26 09:51:05 +0800

    冒泡优化

--- src/main/java/com/from/codetool/tool/Sort.java
+++ src/main/java/com/from/codetool/tool/Sort.java
@@ -15,7 +15,8 @@ public class Sort {
     }
 
     public static void main(String[] args){
-        int[] r = {9, 1, 5, 8, 3, 7, 4, 6, 2};
+        int[] r = {9, 1, 5, 8, 3, 7, 4, 6, 2, 10, 11, 12, 13, 14};
+//        int[] r = {2, 1, 3, 4, 5, 6, 7, 8, 9};
         for(int i : r){
             System.out.print(i);
             System.out.print(",");
@@ -29,41 +30,54 @@ public class Sort {
     }
 
     /**冒泡排序初级版*/
-    private static int[] bubbleSortO(int[] is){
+    private static int[] bubbleSort0(int[] is){
+        int count = 0;
         for(int i = 0; i < is.length; i++){
             for(int j = i + 1; j < is.length; j++){
+                count++;
                 if(is[j] < is[i]){
                     swqp(is, i, j);
                 }
             }
         }
+        System.out.println("count="+count);
         return is;
     }
 
     /**冒泡排序*/
     private static int[] bubbleSort1(int[] is){
+        int count = 0;
         for(int i = 0; i < is.length; i++){
             for(int j = is.length-1; j > i; j--){
+                count++;
                 if(is[j-1] > is[j]){
                     swqp(is, j-1, j);
                 }
             }
         }
+        System.out.println("count="+count);
         return is;
     }
 
     /**冒泡排序优化(可以避免因已经有序的情况下的无意义循环判断。{2,1,3,4,5,6,7,8,9})*/
     private static int[] bubbleSort2(int[] is){
         boolean flag = true;
+        int count = 0;
         for(int i = 0; i < is.length; i++){
+            if(!flag){
+                flag = true;
+                continue;
+            }
             flag = false;
             for(int j = is.length - 1; j > i; j--){
-                if(is[j] < is[i]){
-                    swqp(is, i, j);
+                count++;
+                if(is[j] < is[j-1]){
+                    swqp(is, j-1, j);
                     flag = true;
                 }
             }
         }
+        System.out.println("count="+count);
         return is;
     }
 
